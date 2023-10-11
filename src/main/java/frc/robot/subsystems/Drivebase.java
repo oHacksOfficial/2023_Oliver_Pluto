c6ec7ec4243695718004d149b992cef8a17380e4
oHacksOfficial <olivermfrey@gmail.com>
2023-10-10 19:06:44 -0700

    init

---
+++ src/main/java/frc/robot/subsystems/Drivebase.java
@@ -0,0 +1,35 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drivebase extends SubsystemBase {
+  /** Creates a new Drivebase. */
+  private DifferentialDrive diffDrive;
+  private CANSparkMax leftMotor = new CANSparkMax(0, MotorType.kBrushless);
+  private CANSparkMax rightMotor = new CANSparkMax(0, MotorType.kBrushless);
+ 
+  public Drivebase() {
+
+    diffDrive = new DifferentialDrive(leftMotor, rightMotor);
+
+
+
+  }
+
+
+
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
